63f8fdc200db9ea9073e8f31abe098de5821824e
Pablo Dorrío Vázquez <pablo.dorrio.vazquez@gmail.com>
2023-10-12 22:15:32 +0200

    feat: code display component

---
+++ src/components/CodeDisplay/CodeDisplay.jsx
@@ -0,0 +1,48 @@
+import SyntaxHighlighter from 'react-syntax-highlighter';
+import { atomOneDark } from 'react-syntax-highlighter/dist/esm/styles/hljs';
+import './CodeDisplay.css';
+
+
+
+const Component = () => {
+    const codeString = `
+    def scan_ports(ip: str) -> bool:
+        client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        established = False
+
+        for port in range(1, 65535):
+            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
+                try:
+                    client_socket.settimeout(0.5)
+                    client_socket.connect((ip, port))
+
+                    if not established:
+                        print("PORT\t  STATE\t SERVICE")
+                        established = True
+
+                    open_ports.append(port)
+
+                    try:
+                        service = socket.getservbyport(port)
+                        print(f"{port}/tcp".ljust(9), "open\t", service)
+                    except:
+                        print(f"{port}/tcp".ljust(9), "open\t", "unknown")
+                except:
+                    pass
+
+        return established
+    `
+    return (
+        <div>
+            <div className='code-display-title'>
+                <p>Example code</p>
+                <ion-icon name="clipboard-outline"></ion-icon>
+            </div>
+            <SyntaxHighlighter className="code-display" language="python" style={atomOneDark}>
+                {codeString}
+            </SyntaxHighlighter>
+        </div>
+    );
+};
+
+export default Component;
