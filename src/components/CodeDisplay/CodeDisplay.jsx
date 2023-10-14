b8f7f282bdc60f751cd51894b24c11a076e5381e
Pablo Dorrío Vázquez <pablo.dorrio.vazquez@gmail.com>
2023-10-14 21:25:18 +0200

    feat: add properties to the code display component

--- src/components/CodeDisplay/CodeDisplay.jsx
+++ src/components/CodeDisplay/CodeDisplay.jsx
@@ -4,34 +4,7 @@ import './CodeDisplay.css';
 
 
 
-const Component = () => {
-    const codeString = `
-    def scan_ports(ip: str) -> bool:
-        client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        established = False
-
-        for port in range(1, 65535):
-            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
-                try:
-                    client_socket.settimeout(0.5)
-                    client_socket.connect((ip, port))
-
-                    if not established:
-                        print("PORT\t  STATE\t SERVICE")
-                        established = True
-
-                    open_ports.append(port)
-
-                    try:
-                        service = socket.getservbyport(port)
-                        print(f"{port}/tcp".ljust(9), "open\t", service)
-                    except:
-                        print(f"{port}/tcp".ljust(9), "open\t", "unknown")
-                except:
-                    pass
-
-        return established
-    `
+const Component = ({ codeString }) => {
     return (
         <div>
             <div className='code-display-title'>
