98108cb0338e3a92dbc70b7adf3b1b7519e42fdc
= <pablo.dorrio.vazquez@gmail.com>
2024-09-24 18:16:07 +0200

    fix: progress bar

--- src/components/Continue/Continue.jsx
+++ src/components/Continue/Continue.jsx
@@ -1,5 +1,5 @@
-import './Continue.css'
-import { useLocalStorage } from '../../useLocalStorage'
+import './Continue.css';
+import { useLocalStorage } from '../../useLocalStorage';
 import { Link } from 'react-router-dom';
 
 function Continue() {
@@ -9,24 +9,48 @@ function Continue() {
     const [lessonTitle] = useLocalStorage('lessonTitle', 'Lesson');
     const [lessonPath] = useLocalStorage('lessonPath', '/');
 
+    let totalChapters = 0;
+
+    switch (module) {
+        case 1:
+            totalChapters = 9;
+            break;
+        case 2:
+            totalChapters = 8;
+            break;
+        case 3:
+            totalChapters = 7;
+            break;
+        default:
+            totalChapters = 0;
+    }
+
+    // Calcular el porcentaje de progreso
+    const progressPercentage = totalChapters > 0 ? (lesson / totalChapters) * 100 : 0;
+
     return (
         <div className={`continue-course-container ${module === 0 ? 'd-none' : ''}`}>
             <div className='continue-course'>
                 <div className='continue-course-preview'>
-                    <h6>Course</h6>
+                    <h6>Módulo</h6>
                     <h2>{moduleTitle}</h2>
                     {/* <Link to={lessonPath}>View all chapters<i className='fas fa-chevron-right'></i></Link> */}
                 </div>
                 <div className='continue-course-info'>
                     <div className='progress-container'>
-                        <div className='progress'></div>
+                        <div className='progress'>
+                            <div 
+                                className='progress-filled' 
+                                style={{ width: `${progressPercentage}%` }}
+                            ></div>
+                        </div>
                         <span className='progress-text'>
-                            6/9 Challenges
+                            {lesson} / {totalChapters} Capítulos
                         </span>
                     </div>
-                    <h6>Chapter {lesson}</h6>
+                    <h6>Capítulo {lesson} de {totalChapters}</h6>
                     <h2>{lessonTitle}</h2>
-                    <Link to={lessonPath} className='btn'>Continue</Link>
+                    <Link to={lessonPath} className='btn'>Continuar</Link>
                 </div>
             </div>
         </div>
